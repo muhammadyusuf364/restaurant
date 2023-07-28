aff6299edc921de6697959b052d1495bd458e170
muhammadyusuf364 <mirzayevmuhammadyusuf359@gmail.com>
2023-07-28 14:59:09 +0500

    lorem

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos similique aliquam fugit commodi repellendus dolorum natus est optio fuga cumque fugiat veritatis suscipit debitis, ullam porro vel? Quo, consequuntur dicta. Ipsam, pariatur quibusdam. Mollitia obcaecati vel harum, sunt quae necessitatibus asperiores? Illum fuga quibusdam ratione sit quo? Voluptatem placeat impedit quidem culpa eius blanditiis! Accusamus quidem impedit soluta eius qui quos repellendus! Quam quidem expedita dolorem mollitia ad dolorum facilis vel quaerat, deserunt iste, ipsa esse magni rem pariatur porro odio, quas sit totam. Nam, libero repellat fugiat, ipsam illo ipsum, dolor aliquid quia veritatis dolores itaque qui aspernatur nihil!</p>
+    </div>
   )
 }
 
